1d723373d86471b84e5269d525101711888bf4a8
Jason Dudash <thenut77@gmail.com>
2025-06-26 09:52:55 -0400

    mdbub 0.3.10
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mdbub.rb
+++ Formula/mdbub.rb
@@ -1,8 +1,8 @@
 class Mdbub < Formula
   desc "Terminal-first interactive mindmap CLI tool"
   homepage "https://github.com/collabinator/mdbubbles"
-  url "https://files.pythonhosted.org/packages/source/m/mdbub/mdbub-0.3.0.tar.gz"
-  sha256 "WILL BE SET BY GITHUB ACTIONS"
+  url "https://github.com/collabinator/mdbubbles/archive/refs/tags/v0.3.10.tar.gz"
+  sha256 "2df401574dc22c22f1ff9f9deb1e7b6e4f3ed59e40d5bcbc89b5d3ee68e0d763"
   license "Apache-2.0"
 
   depends_on "python@3.11"
